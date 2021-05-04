2880387678080647cde4eb1fdf071464a889242e
Mustafa YEŞİL <mustafayesil518@gmail.com>
2021-05-04 15:52:53 +0300

    add project files

---
+++ homeWork/src/homeWork/Course.java
@@ -0,0 +1,21 @@
+package homeWork;
+
+public class Course {
+	
+	public Course() {
+		
+	}
+	public Course(int id, String name, String instructorName, int progress) {
+		this();
+		this.id=id;
+		this.name=name;
+		this.instructorName=instructorName;
+		this.progress=progress;
+	}
+	
+	
+	int id;
+	String name;
+	String instructorName;
+	int progress;
+}
---
+++ homeWork/src/homeWork/CourseManager.java
@@ -0,0 +1,7 @@
+package homeWork;
+
+public class CourseManager {
+	public void addCourse(Course course) {
+		System.out.println("Kurs Eklendi : "+course.name);
+	}
+}
